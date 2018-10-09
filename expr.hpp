4125202e2ed015e6f4995b80793fc77d26a46bb4
nfgallimore <nfgallimore@gmail.com>
2018-10-09 18:12:38 -0400

    Added inline constructors and more comments to expr.hpp

--- expr.hpp
+++ expr.hpp
@@ -13,8 +13,8 @@ class Value;
 class Expr 
 {
 public:
-    /// Evaluates the expression.
     virtual Value evaluate() const = 0;
+    /// Evaluates the expression.
 
     virtual void print(Printer& p) const = 0;
     /// `Pretty prints` the expression.
@@ -45,6 +45,12 @@ public:
 
     Value evaluate() const override;
     /// Evaluates the expression.
+
+    bool get_bool() { return m_value; }
+    /// Returns the boolean value of the boolean literal expression.
+
+    Type* get_type() { return m_type; }
+    /// Returns the type of the boolean literal expression.
 
 private:
     bool m_value;
@@ -371,7 +377,7 @@ class Eq_expr : public Expr
 {
 public:
     Eq_expr(Expr* e1, Expr* e2, Type* t);
-    /// Constructs the equality expression.
+    /// Constructs an equality expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -466,10 +472,9 @@ Value Ne_expr::evaluate() const
 class Lt_expr : public Expr
 {
 public:
-    Lt_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
-
+    Lt_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a less than expression with the given arguments.
+     
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
 
@@ -479,23 +484,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the less than expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the less than expression expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the less than expression expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the less than expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the less than expression.
+
     Type* m_type;
+    /// The type of the less than expression.
 };
+
+inline
+Lt_expr::Lt_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Lt_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the greater than expression of the form `e1 > e2`.
 class Gt_expr : public Expr
 {
 public:
-    Gt_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Gt_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a greater than expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -506,22 +532,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the greater than expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the greater than expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the greater than expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the greater than expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the greater than expression.
+
     Type* m_type;
+    /// The type of the greater than expression.
 };
+
+inline
+Gt_expr::Gt_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Gt_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
+
 
 /// Represents the less than or equal expression of the form `e1 <= e2`.
 class Le_expr : public Expr
 {
 public:
-    Le_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Le_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a less than or equal expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -532,23 +580,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the less than or equal expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the less than or equal expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the less than or equal expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the less than or equal expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the less than or equal expression.
+
     Type* m_type;
+    /// The type of the less than or equal expression.
 };
+
+inline
+Le_expr::Le_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Le_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the greater than or equal expression of the form `e1 >= e2`.
 class Ge_expr : public Expr 
 {
 public:
-    Ge_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Ge_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a greater than or equal expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -559,23 +628,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the greater than or equal expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the greater than or equal expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the greater than or equal expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the greater than or equal expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the greater than or equal expression.
+
     Type* m_type;
+    /// The type of the greater than or equal expression.
 };
+
+inline
+Ge_expr::Ge_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Ge_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the addition expression of the form `e1 + e2`.
 class Add_expr : public Expr
 {
 public:
-    Add_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Add_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs an add expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -586,23 +676,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the addition expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the addition expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the addition expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the addition expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the addition expression.
+
     Type* m_type;
+    /// The type of the addition expression.
 };
+
+inline
+Add_expr::Add_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Add_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the subtraction expression of the form `e1 - e2`.
 class Sub_expr : public Expr 
 {
 public:
-    Sub_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Sub_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a subtraction expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -613,23 +724,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the subtraction expression.
+    
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the subtraction expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the subtraction expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the subtraction expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the subtraction expression.
+
     Type* m_type;
+    /// The type of the subtraction expression.
 };
+
+inline
+Sub_expr::Sub_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Sub_expr::evaluate() const
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the multiplication expression of the form `e1 * e2`.
 class Mul_expr : public Expr
 {
 public:
-    Mul_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Mul_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a multiplication expression with the given arugments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -640,23 +772,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the multiplication expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the multiplication expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the multiplication expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the multiplication expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the multiplication expression.
+
     Type* m_type;
+    /// The type of the multiplication expression.
 };
+
+inline
+Mul_expr::Mul_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Mul_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the quotient expression of the form `e1 / e2`.
 class Quo_expr : public Expr
 {
 public:
-    Quo_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Quo_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a quotient expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -667,23 +820,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the quotient expression.
+    
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the quotient expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the quotient expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the quotient expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the quotient expression.
+
     Type* m_type;
+    /// The type of the quotient expression.
 };
+
+inline
+Quo_expr::Quo_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Quo_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the remainder expression of the form `e1 % e2`.
 class Rem_expr : public Expr
 {
 public:
-    Rem_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Rem_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs a remainder expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -694,23 +868,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the remainder expression.
+
+    Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the remainder expression.
+
+    Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the remainder expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the remainder expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the remainder expression.
+
     Type* m_type;
+    /// The type of the remainder expression.
 };
+
+inline
+Rem_expr::Rem_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Rem_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 // Represents the negation expression of the form `-e1`.
 class Neg_expr : public Expr
 {
 public:
-    Neg_expr(Expr* e, Type* t) 
-        : m_expr(e), m_type(t) 
-    { }
+    Neg_expr(Expr* e, Type* t);
+    /// Constructs a negation expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -721,22 +916,38 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the negation expression.
+
+    Expr* get_expr() { return m_expr; }
+    /// Gets the expression being negated
+
 private:
     Expr* m_expr;
+    /// The expression being negated.
+
     Type* m_type;
+    /// The type of the negation expression.
 };
+
+inline
+Neg_expr::Neg_expr(Expr* e, Type* t) 
+    : m_expr(e), m_type(t) 
+{ }
+
+inline
+Value Neg_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the reciprocal expression of the form `/e1`.
 class Rec_expr : public Expr 
 {
 public:
-    Rec_expr(Expr* e, Type* t) 
-        : m_expr(e), m_type(t) 
-    { }
+    Rec_expr(Expr* e, Type* t);
+    /// Constructs a reciprocal expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -747,22 +958,38 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the reciprocal expression.
+
+    Expr* get_expr() { return m_expr; }
+    /// Gets the expression the reciprocal is being applied to.
+
 private:
     Expr* m_expr;
+    /// The expression the reciprocal is being applied to.
+
     Type* m_type;
+    /// The type of the reciprocal expression.
 };
+
+inline
+Rec_expr::Rec_expr(Expr* e, Type* t) 
+    : m_expr(e), m_type(t) 
+{ }
+
+inline
+Value Rec_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the assignment expression of the form `e1 <- e2`.
 class Ass_expr : public Expr
 {
 public:
-    Ass_expr(Expr* src, Expr* tar, Type* t)
-        : m_src(src), m_tar(tar), m_type(t)
-    { }
+    Ass_expr(Expr* src, Expr* tar, Type* t);
+    /// Constructs an assignment expression with the given arguments.
     
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -773,23 +1000,47 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the assignment expression.
+
+    Expr* get_src() { return m_src; }
+    /// Gets the source expression.
+    
+    Expr* get_tar() { return m_tar; }
+    /// Gets the target expression.
+
+    Type* get_type() { return m_type; }
+    /// Returns the type of the assignmente expression.
+
 private:
     Type* m_type;
+    /// The type of the assignment expression.
+
     Expr* m_src;
+    /// The expression source of the assignment expression.
+
     Expr* m_tar;
+    /// The expression target of the assignment expression.
 };
+
+inline
+Ass_expr::Ass_expr(Expr* src, Expr* tar, Type* t)
+    : m_src(src), m_tar(tar), m_type(t)
+{ }
+
+inline
+Value Ass_expr::evaluate() const 
+{
+    throw std::logic_error("Not implemented");
+}
 
 
 /// Represents the function call expression of the form `ef(e1, e2, ..., en)`.
 class Call_expr : public Expr
 {
 public:
-    Call_expr(std::vector<Expr*>* exprs, Type* t)
-        : m_exprs(exprs), m_type(t)
-    { }
+    Call_expr(std::vector<Expr*>* exprs, Type* t);
+    /// Constructs a function call expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -800,13 +1051,33 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        throw std::logic_error("Not implemented");
-    }
+    Value evaluate() const override;
+    /// Evaluates the function call expression.
+
+    std::vector<Expr*>* get_exprs() { return m_exprs; }
+    /// Returns the parameters of the function call.
+
+    Type* get_type() { return m_type; }
+    /// Returns the type of the function call.
+
 private:
     Type* m_type;
+    /// Tye type of the function call.
+
     std::vector<Expr*>* m_exprs;
+    /// The parameters of the function call.
 };
+
+inline
+Call_expr::Call_expr(std::vector<Expr*>* exprs, Type* t)
+    : m_exprs(exprs), m_type(t)
+{ }
+
+inline
+Value Call_expr::evaluate() const 
+{
+    throw std::logic_error("Cannot evaluate a function call expression.");
+}
 
 
 // Operators
