394847a23333b54e4701ba438bc26bbd25b30f77
nfgallimore <nfgallimore@gmail.com>
2018-10-09 17:20:28 -0400

    Added more member comments for expr.hpp

--- expr.hpp
+++ expr.hpp
@@ -43,7 +43,7 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override { return Value(m_value); }
+    Value evaluate() const override;
     /// Evaluates the expression.
 
 private:
@@ -59,14 +59,19 @@ Bool_lit::Bool_lit(bool b, Type* t)
     : m_value(b), m_type(t) 
 { }
 
+inline
+Value Bool_lit::evaluate() const
+{
+    return Value(m_value);
+}
 
-/// Represents the integer literal of the form `1, 2, ..., n`
+
+/// Represents the integer literal of the form `1, 2, ..., n`.
 class Int_lit : public Expr 
 {
 public:    
-    Int_lit(int i, Type* t) 
-        : m_value(i), m_type(t) 
-    { }
+    Int_lit(int i, Type* t);
+    /// Constructs an integer literal expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -77,22 +82,35 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override { return Value(m_value); }
+    Value evaluate() const override;
     /// Evaluates the expression.
 
 private:
     int m_value;
+    /// The integer value of the integer literal expression.
+
     Type* m_type;
+    /// The type of the integer litearl expression.
 };
+
+inline
+Int_lit::Int_lit(int i, Type* t) 
+    : m_value(i), m_type(t) 
+{ }
+
+inline
+Value Int_lit::evaluate() const
+{
+    return Value(m_value);
+}
 
 
 /// Represents the float literal of the form `1, 1.1, ..., n`.
 class Float_lit : public Expr
 {
 public:    
-    Float_lit(float f, Type* t) 
-        : m_value(f), m_type(t) 
-    { }
+    Float_lit(float f, Type* t);
+    /// Constructs a float literal expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -103,22 +121,35 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override { return Value(m_value); }
+    Value evaluate() const override;
     /// Evaluates the expression.
 
 private:
     float m_value;
+    /// The float value of the float literal expression.
+
     Type* m_type;
+    /// The type of the float literal expression.
 };
+
+inline
+Float_lit::Float_lit(float f, Type* t) 
+    : m_value(f), m_type(t) 
+{ }
+
+inline
+Value Float_lit::evaluate() const
+{
+    return Value(m_value);
+}
 
 
 /// Represents the identifier expression of the form `x`.
 class Id_expr : public Expr
 {
 public:
-    Id_expr(Decl* d, Type* t) 
-        : m_value(d), m_type(t) 
-    { }
+    Id_expr(Decl* d, Type* t);
+    /// Constructs an identifier expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -129,20 +160,35 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override { throw std::logic_error("Cannot evaluate an identifier."); }
+    Value evaluate() const override;
     /// Evaluates the expression.
 
 private:
     Decl* m_value;
+    /// The declaration of the identifier expression.
+
     Type* m_type;
+    /// The type of the identifier expression.
 };
+
+inline
+Id_expr::Id_expr(Decl* d, Type* t) 
+    : m_value(d), m_type(t) 
+{ }
+
+inline
+Value Id_expr::evaluate() const
+{
+    throw std::logic_error("Cannot evaluate an identifier.");
+}
 
 
 /// Represents the logical AND expression of the form `e1 and e2`.
 class And_expr : public Expr 
 {
 public:
-    And_expr(Expr* e1, Expr* e2, Type* t) : m_e1(e1), m_e2(e2), m_type(t) { }
+    And_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs an and expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -153,27 +199,44 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        return Value(m_e1->evaluate().get_bool() && m_e2->evaluate().get_bool());
-    }
+    Value evaluate() const override;
     /// Evaluates the expression.
 
     Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side expression of the and expression.
+
     Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side expression of the and expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the and expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the and expression.
+
     Type* m_type;
+    /// The type of the and expression.
 };
+
+inline
+And_expr::And_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value And_expr::evaluate() const
+{
+    return Value(m_e1->evaluate().get_bool() && m_e2->evaluate().get_bool());
+}
 
 
 /// Represents the logical OR expression of the form `e1 or e2`.
 class Or_expr : public Expr 
 {
 public:
-    Or_expr(Expr* e1, Expr* e2, Type* t) 
-        : m_e1(e1), m_e2(e2), m_type(t) 
-    { }
+    Or_expr(Expr* e1, Expr* e2, Type* t);
+    /// Constructs an or expression with the given arguments.
 
     void print(Printer& p) const override;
     /// `Pretty prints` the expression.
@@ -184,16 +247,36 @@ public:
     void to_sexpr(Printer& p) const override;
     /// Prints the expression as a symbolic expression.
 
-    Value evaluate() const override {
-        return Value(m_e1->evaluate().get_bool() || m_e2->evaluate().get_bool());
-    }
+    Value evaluate() const override;
+    /// Evaluates the or expression.
+
     Expr* get_lhs() { return m_e1; }
+    /// Gets the left hand side of the or expression.
+
     Expr* get_rhs() { return m_e2; }
+    /// Gets the right hand side of the or expression.
+
 private:
     Expr* m_e1;
+    /// The left hand side expression of the or expression.
+
     Expr* m_e2;
+    /// The right hand side expression of the or expression.
+
     Type* m_type;
+    /// The type of the or expression.
 };
+
+inline
+Or_expr::Or_expr(Expr* e1, Expr* e2, Type* t) 
+    : m_e1(e1), m_e2(e2), m_type(t) 
+{ }
+
+inline
+Value Or_expr::evaluate() const
+{
+    return Value(m_e1->evaluate().get_bool() || m_e2->evaluate().get_bool());
+}
 
 
 /// Represents the logical NOT expression of the form `not e1`.
@@ -622,6 +705,7 @@ private:
     Expr* m_src;
     Expr* m_tar;
 };
+
 
 /// Represents the function call expression of the form `ef(e1, e2, ..., en)`.
 class Call_expr : public Expr
